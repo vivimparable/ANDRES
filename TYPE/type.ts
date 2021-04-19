7b102299a6541e182b4098617b3ec460f1aba69c
Javier <javirgarciaurruchi@gmail.com>
2021-04-19 21:50:51 +0200

    Cosa

---
+++ TYPE/type.ts
@@ -0,0 +1,8 @@
+function multiplicar(numero1:number, numero2:number){
+let resultado:number = numero1 * numero2;
+
+(<HTMLInputElement>document.getElementById("info")).innerHTML=  resultado;
+}
+
+
+
